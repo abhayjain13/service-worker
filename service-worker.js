625e5eb01bfa8e5c124dce0e3713cf0c03a81e0e
Abhay Jain <abhayjain13@users.noreply.github.com>
2020-12-28 20:55:24 +0530

    Update Service Worker

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,7 @@ const PRECACHE = "v1";
 const RUNTIME = "runtime";
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  "index.html",
-  "./", // Alias for index.html
-  "sw.js",
-];
+const PRECACHE_URLS = ["index.html", "sw.js", "service-worker.js"];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener("install", (event) => {
